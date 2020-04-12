988362e5e63395b3cf44351808eecb7bca932d05
mellymillionz <melissamunz@gmail.com>
2020-04-12 11:42:26 -0400

    completed funnel for 3 tables

--- Build_a_Funnel.sql
+++ Build_a_Funnel.sql
@@ -130,4 +130,28 @@ SUM(is_purchase) as num_purchase,
 FROM funnels;
 
 -- The management team suspects that conversion from checkout to purchase changes as the browse_date gets closer to Christmas Day.
+-- added browse_date as the fist column in non subquery and then grouped by that column
 
+WITH funnels AS (
+  SELECT DISTINCT b.browse_date,
+     b.user_id,
+     c.user_id IS NOT NULL AS 'is_checkout',
+     p.user_id IS NOT NULL AS 'is_purchase'
+  FROM browse AS 'b'
+  LEFT JOIN checkout AS 'c'
+    ON c.user_id = b.user_id
+  LEFT JOIN purchase AS 'p'
+    ON p.user_id = c.user_id)
+SELECT browse_date,
+COUNT(*) AS 'num_browse',
+   SUM(is_checkout) AS 'num_checkout',
+   SUM(is_purchase) AS 'num_purchase',
+   1.0 * SUM(is_checkout) / COUNT(user_id) AS 'browse_to_checkout',
+   1.0 * SUM(is_purchase) / SUM(is_checkout) AS 'checkout_to_purchase'
+FROM funnels
+GROUP BY browse_date
+ORDER BY browse_date;
+
+-- ABOVE Conversion from checkout to purchase increases from 80% on 12/20 to 94% on 12/23!
+
+-- In this lesson, we learned how to build a funnel from a single table, compare funnels for A/B tests, and create a funnel from multiple tables using LEFT JOIN.
