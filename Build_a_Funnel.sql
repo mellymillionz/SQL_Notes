07f4ed0f8200fb8aec9eeee5d299ae7ed0e19310
mellymillionz <melissamunz@gmail.com>
2020-04-12 11:10:24 -0400

    AB testing with funnels

--- Build_a_Funnel.sql
+++ Build_a_Funnel.sql
@@ -12,3 +12,21 @@ GROUP BY 1;
 -- You could look at differences in completion rates between questions to determine which kept people from moving forward with the survey.
 
 -- Compare funnels for AB Testing:
+-- How is the funnel different between the two groups?
+
+SELECT modal_text, 
+   COUNT(DISTINCT user_id)
+FROM onboarding_modals
+GROUP BY 1
+ORDER BY 1;
+
+-- We can use a CASE statement within our COUNT() aggregate so that we only count user_ids whose ab_group is equal to ‘control’:
+
+SELECT modal_text,
+COUNT(DISTINCT CASE
+     WHEN ab_group = 'control' THEN
+     user_id
+     END) AS 'control_clicks'
+FROM onboarding_modals
+GROUP BY 1
+ORDER BY 1;
